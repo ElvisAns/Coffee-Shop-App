17445e0a99970d21e6d421b7df0f0cf84398b640
ElviAns <ansimapersic@gmail.com>
2022-09-29 01:32:54 +0200

    Updated the app secret (this file shouldn't have been shared)

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'dev-f-c9bg6g.us.auth0.com', // the auth0 domain prefix
+    audience: 'https://127.0.0.1:8100', // the audience set for the auth0 app
+    clientId: 'b4HcPNHQv60YYwxdTcV4vPFTnFoRsnft', // the client id generated for the auth0 app
+    callbackURL: 'https://127.0.0.1:8100', // the base url of the running ionic application. 
   }
 };
