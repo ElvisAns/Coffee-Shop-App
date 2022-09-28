20dd35bfffff4e0ed4d58ae5fb48936d8cd597a6
ElviAns <ansimapersic@gmail.com>
2022-09-29 01:32:24 +0200

    Fix some issue with the frontend

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -22,12 +22,12 @@ export class AuthService {
 
   build_login_link(callbackPath = '') {
     let link = 'https://';
-    link += this.url + '.auth0.com';
+    link += this.url;
     link += '/authorize?';
+    link += 'redirect_uri=' + this.callbackURL + "&";
     link += 'audience=' + this.audience + '&';
     link += 'response_type=token&';
-    link += 'client_id=' + this.clientId + '&';
-    link += 'redirect_uri=' + this.callbackURL + callbackPath;
+    link += 'client_id=' + this.clientId ;
     return link;
   }
 
