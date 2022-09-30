dfed86f4398d9a8dbf27443144469b68b2e74b42
ElviAns <ansimapersic@gmail.com>
2022-09-30 14:15:48 +0200

    Fix issue with flask run

--- backend/src/api.py
+++ backend/src/api.py
@@ -4,8 +4,8 @@ from flask import Flask, request, jsonify, abort
 from sqlalchemy import exc
 import json
 from flask_cors import CORS
-from database.models import db_drop_and_create_all, setup_db, Drink
-from auth.auth import AuthError, requires_auth
+from .database.models import db_drop_and_create_all, setup_db, Drink
+from .auth.auth import AuthError, requires_auth
 
 app = Flask(__name__)
 setup_db(app)
