409c1d9eaa2ed6cc44f5fe1cf4cb56f6bcca7f94
ElviAns <ansimapersic@gmail.com>
2022-09-30 16:10:47 +0200

    Properly format the environment variable value

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-f-c9bg6g.us.auth0.com', // the auth0 domain prefix
+    url: 'dev-f-c9bg6g.us', // the auth0 domain prefix
     audience: 'https://127.0.0.1:8100', // the audience set for the auth0 app
     clientId: 'b4HcPNHQv60YYwxdTcV4vPFTnFoRsnft', // the client id generated for the auth0 app
     callbackURL: 'https://127.0.0.1:8100', // the base url of the running ionic application. 
